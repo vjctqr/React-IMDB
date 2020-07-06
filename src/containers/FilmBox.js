32752480393b998365078905d4343152d4b7968c
Victor <victorsetlhare@gmail.com>
2020-07-06 21:41:01 +0100

    filmbox construct

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class FilmBox extends Component{
+    construct(props){
+        super(props);
+        this.state = {
+            films: [
+                {
+                    id: 1,
+                    name: "Spider-Man: Into the Spider-Verse",
+                    url: "http://www.imdb.com/title/tt4633694/?ref_=rlm"
+                }
+            ]
+        }
+    }
+}
